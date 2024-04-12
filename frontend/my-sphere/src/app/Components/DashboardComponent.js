fb6fc129bb455ddf06cf2f6b4a37ea113711d07a
cfossto <70951673+cfossto@users.noreply.github.com>
2024-04-12 23:33:55 +0200

    Multi-component injection

---
+++ frontend/my-sphere/src/app/Components/DashboardComponent.js
@@ -0,0 +1,37 @@
+function Test(){
+
+    let str = "hello"
+    return str
+}
+
+const fs = require('fs')
+const toml = require('toml')
+let data = fs.readFileSync('/Users/christopherfossto/Desktop/my-sphere/my-sphere/frontend/my-sphere/src/app/config/config.toml', 'utf-8')
+let res = toml.parse(data)
+
+let newlist = []
+
+for (let c of res.hosts){
+  newlist.push(
+    <div className="dashboard-area mt-10">
+    <h2 className="mt-5 ml-5">Dashboard-area</h2>
+        <ul>
+            <li className='ml-5' key={c.host}>{ c.url }</li>
+        </ul>
+    </div>
+  )
+
+}
+
+export default function Dashboard() {
+
+    return (
+
+        <div>
+            { newlist }
+        </div>
+
+    )
+
+
+}
--- frontend/my-sphere/src/app/page.js
+++ frontend/my-sphere/src/app/page.js
@@ -1,17 +1,4 @@
-const fs = require('fs')
-const toml = require('toml')
-let data = fs.readFileSync('/Users/christopherfossto/Desktop/my-sphere/my-sphere/frontend/my-sphere/src/app/config/config.toml', 'utf-8')
-let res = toml.parse(data)
-
-console.log(typeof(res))
-console.log(res)
-
-let newlist = []
-
-for (let c of res.hosts){
-  newlist.push(<li>{ c.url }</li>)
-}
-
+import  Dashboard  from './Components/DashboardComponent.js'
 
 
 export default function Home() {
@@ -21,26 +8,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
 
+    <div>
       <h1 className="mt-10">MY SPHERE</h1>
+    </div>
 
-      <div className="dashboard-area mt-10">
-        <h2 className="mt-5 ml-5">Dashboard-area 1</h2>
-        <ul>
-          <li>test</li>
-          <li>
-            { newlist }
-          </li>
-        </ul>
-      </div>
+    <div>
+        <Dashboard />
+    </div>
 
-      <div className="dashboard-area mt-10">
-        <h2 className="mt-5 ml-5">Dashboard-area 2</h2>
-
-      </div>
-
-      <div className="dashboard-area mt-10">
-        <h2 className="mt-5 ml-5">Dashboard-area 3</h2>
-      </div>
 
     </main>
   );
