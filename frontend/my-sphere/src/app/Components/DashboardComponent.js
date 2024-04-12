ab4eb1b862e9b7482ea42e328ad4db094fa34c6d
cfossto <70951673+cfossto@users.noreply.github.com>
2024-04-12 23:36:20 +0200

    Cleanup plus key

--- frontend/my-sphere/src/app/Components/DashboardComponent.js
+++ frontend/my-sphere/src/app/Components/DashboardComponent.js
@@ -1,9 +1,3 @@
-function Test(){
-
-    let str = "hello"
-    return str
-}
-
 const fs = require('fs')
 const toml = require('toml')
 let data = fs.readFileSync('/Users/christopherfossto/Desktop/my-sphere/my-sphere/frontend/my-sphere/src/app/config/config.toml', 'utf-8')
@@ -13,25 +7,20 @@ let newlist = []
 
 for (let c of res.hosts){
   newlist.push(
-    <div className="dashboard-area mt-10">
+    <div key={c.host} className="dashboard-area mt-10">
     <h2 className="mt-5 ml-5">Dashboard-area</h2>
         <ul>
-            <li className='ml-5' key={c.host}>{ c.url }</li>
+            <li className='ml-5'>{ c.url }</li>
         </ul>
     </div>
   )
-
 }
 
 export default function Dashboard() {
 
     return (
-
         <div>
             { newlist }
         </div>
-
     )
-
-
 }
--- frontend/my-sphere/src/app/page.js
+++ frontend/my-sphere/src/app/page.js
@@ -1,22 +1,17 @@
 import  Dashboard  from './Components/DashboardComponent.js'
 
-
 export default function Home() {
-
-  
 
   return (
     <main className="flex min-h-screen flex-col items-center">
 
-    <div>
-      <h1 className="mt-10">MY SPHERE</h1>
-    </div>
+      <div>
+        <h1 className="mt-10">MY SPHERE</h1>
+      </div>
 
-    <div>
+      <div>
         <Dashboard />
-    </div>
-
-
+      </div>
     </main>
   );
 }
