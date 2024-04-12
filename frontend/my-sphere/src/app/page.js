ad8cdd5531d3ed417e8e6639eff389540334e9d5
cfossto <70951673+cfossto@users.noreply.github.com>
2024-04-12 17:54:26 +0200

    Can put list on DOM

--- frontend/my-sphere/src/app/page.js
+++ frontend/my-sphere/src/app/page.js
@@ -3,11 +3,20 @@ const toml = require('toml')
 let data = fs.readFileSync('/Users/christopherfossto/Desktop/my-sphere/my-sphere/frontend/my-sphere/src/app/config/config.toml', 'utf-8')
 let res = toml.parse(data)
 
-let j = JSON.parse(JSON.stringify(res))
+console.log(typeof(res))
+console.log(res)
 
-console.log(j.hosts)
+let newlist = []
+
+for (let c of res.hosts){
+  newlist.push(<li>{ c.url }</li>)
+}
+
+
 
 export default function Home() {
+
+  
 
   return (
     <main className="flex min-h-screen flex-col items-center">
@@ -17,11 +26,16 @@ export default function Home() {
       <div className="dashboard-area mt-10">
         <h2 className="mt-5 ml-5">Dashboard-area 1</h2>
         <ul>
+          <li>test</li>
+          <li>
+            { newlist }
+          </li>
         </ul>
       </div>
 
       <div className="dashboard-area mt-10">
         <h2 className="mt-5 ml-5">Dashboard-area 2</h2>
+
       </div>
 
       <div className="dashboard-area mt-10">
